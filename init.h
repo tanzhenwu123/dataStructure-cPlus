75ac291145177ba72f2848bf7bb636d6227cd3dd
tazhenwu123 <1105049762@qq.com>
2023-12-06 18:19:46 +0800

    init.h add Array Template

--- init.h
+++ init.h
@@ -2,15 +2,18 @@
 // Created by Lenovo on 2023/12/2.
 //
 
+#include "iostream"
+#include "vector"
+
 #ifndef DATASTRUCTURE_CPLUS_INIT_H
 #define DATASTRUCTURE_CPLUS_INIT_H
 
-#include "iostream"
-#include "vector"
-using Array = std::vector<int>;
+#pragma once
+template<typename T>
+using Array = std::vector<T>;
 
-Array insertSort(Array array);
-Array mergeSort(Array array);
-int binarySearch(Array array, int begin, int end, int target);
+Array<int> insertSort(Array<int> array);
+Array<int> mergeSort(Array<int> array);
+int binarySearch(Array<int> array, int begin, int end, int target);
 
 #endif //DATASTRUCTURE_CPLUS_INIT_H
--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 #include "init.h"
 
-Array insertArray = {2,9,0,11,3};
-Array mergeArray = {20,13,7,2,12,11,9,1};
+Array<int> insertArray = {2,9,0,11,3};
+Array<int> mergeArray = {20,13,7,2,12,11,9,1};
 
 int main() {
-    Array sortResult1 = insertSort(insertArray);
-    Array sortResult2 = mergeSort(mergeArray);
+    Array<int> sortResult1 = insertSort(insertArray);
+    Array<int> sortResult2 = mergeSort(mergeArray);
     int searchResult1 = binarySearch(sortResult2, 0, sortResult2.size() - 1, 9);
     std::cout<< searchResult1 << std::endl;
     return 0;
